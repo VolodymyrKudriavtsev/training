a3980d45ea62abfd2549f7f9a72339efb256e899
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-10-11 23:29:34 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,37 +1,26 @@
-const atTheOldToad = {
-	potions: [
-		{ name: "Speed potion", price: 460 },
-		{ name: "Dragon breath", price: 780 },
-		{ name: "Stone skin", price: 520 },
-	],
+function changeEven(numbers, value) {
 	// Change code below this line
-	getPotions() {
-		return console.log(this.potions);
-	},
-	addPotion(newPotion) {
-		if (this.potions.includes(newPotion)) {
-			return `Error! Potion ${newPotion} is already in your inventory!`;
+	const newArray = [];
+
+	// for (let i = 0; i < numbers.length; i += 1) {
+	// 	if (numbers[i] % 2 === 0) {
+	// 		numbers[i] = numbers[i] + value;
+	// 	}
+	// }
+
+	numbers.forEach(number => {
+		if (number % 2 === 0) {
+			newArray.push(number + value);
+		} else {
+			newArray.push(number);
 		}
+	});
 
-		this.potions.push(newPotion);
-	},
-	removePotion(potionName) {
-		const potionIndex = this.potions.indexOf(potionName);
-
-		if (potionIndex === -1) {
-			return `Potion ${potionName} is not in inventory!`;
-		}
-
-		this.potions.splice(potionIndex, 1);
-	},
-	updatePotionName(oldName, newName) {
-		const potionIndex = this.potions.indexOf(oldName);
-
-		if (potionIndex === -1) {
-			return `Potion ${oldName} is not in inventory!`;
-		}
-
-		this.potions.splice(potionIndex, 1, newName);
-	},
+	return console.log(newArray);
 	// Change code above this line
-};
+}
+
+changeEven([1, 2, 3, 4, 5], 10); //повертає новий масив [1, 12, 3, 14, 5]
+changeEven([2, 8, 3, 7, 4, 6], 10); //повертає новий масив [12, 18, 3, 7, 14, 16]
+changeEven([17, 24, 68, 31, 42], 100); //повертає новий масив [17, 124, 168, 31, 142]
+changeEven([44, 13, 81, 92, 36, 54], 100); //повертає новий масив [144, 13, 81, 192, 136, 154
