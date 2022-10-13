310ab3f108a6b4ece7050bf846b46edb03153abc
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-10-14 01:24:42 +0300

    14.10.2022

--- js/main.js
+++ js/main.js
@@ -1,26 +1,75 @@
-function changeEven(numbers, value) {
-	// Change code below this line
-	const newArray = [];
+const users = [
+	{
+		name: "Moore Hensley",
+		email: "moorehensley@indexia.com",
+		eyeColor: "blue",
+		friends: ["Sharron Pace"],
+		isActive: false,
+		balance: 2811,
+		gender: "male",
+	},
+	{
+		name: "Sharlene Bush",
+		email: "sharlenebush@tubesys.com",
+		eyeColor: "blue",
+		friends: ["Briana Decker", "Sharron Pace"],
+		isActive: true,
+		balance: 3821,
+		gender: "female",
+	},
+	{
+		name: "Ross Vazquez",
+		email: "rossvazquez@xinware.com",
+		eyeColor: "green",
+		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+		isActive: false,
+		balance: 3793,
+		gender: "male",
+	},
+	{
+		name: "Elma Head",
+		email: "elmahead@omatom.com",
+		eyeColor: "green",
+		friends: ["Goldie Gentry", "Aisha Tran"],
+		isActive: true,
+		balance: 2278,
+		gender: "female",
+	},
+	{
+		name: "Carey Barr",
+		email: "careybarr@nurali.com",
+		eyeColor: "blue",
+		friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+		isActive: true,
+		balance: 3951,
+		gender: "male",
+	},
+	{
+		name: "Blackburn Dotson",
+		email: "blackburndotson@furnigeer.com",
+		eyeColor: "brown",
+		friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+		isActive: false,
+		balance: 1498,
+		gender: "male",
+	},
+	{
+		name: "Sheree Anthony",
+		email: "shereeanthony@kog.com",
+		eyeColor: "brown",
+		friends: ["Goldie Gentry", "Briana Decker"],
+		isActive: true,
+		balance: 2764,
+		gender: "female",
+	},
+];
 
-	// for (let i = 0; i < numbers.length; i += 1) {
-	// 	if (numbers[i] % 2 === 0) {
-	// 		numbers[i] = numbers[i] + value;
-	// 	}
-	// }
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) =>
+	[...users].filter(user => user.gender === gender).reduce((totalBalance, user) => totalBalance + user.balance, 0);
+// Change code above this line
 
-	numbers.forEach(number => {
-		if (number % 2 === 0) {
-			newArray.push(number + value);
-		} else {
-			newArray.push(number);
-		}
-	});
+console.log(getTotalBalanceByGender(users, "male")); //12053
+console.log(getTotalBalanceByGender(users, "female")); //8863
 
-	return console.log(newArray);
-	// Change code above this line
-}
-
-changeEven([1, 2, 3, 4, 5], 10); //повертає новий масив [1, 12, 3, 14, 5]
-changeEven([2, 8, 3, 7, 4, 6], 10); //повертає новий масив [12, 18, 3, 7, 14, 16]
-changeEven([17, 24, 68, 31, 42], 100); //повертає новий масив [17, 124, 168, 31, 142]
-changeEven([44, 13, 81, 92, 36, 54], 100); //повертає новий масив [144, 13, 81, 192, 136, 154
+// повертала загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
